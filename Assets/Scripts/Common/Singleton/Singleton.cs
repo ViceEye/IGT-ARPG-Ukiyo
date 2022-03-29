732cdffc70fbe303d1b703a7c99040ddfe6508fd
ViceEye <ckh985169151@qq.com>
2022-03-29 23:39:03 +0100

    Singleton

---
+++ Assets/Scripts/Common/Singleton/Singleton.cs
@@ -0,0 +1,36 @@
+﻿namespace Ukiyo.Common.Singleton
+{
+    public abstract class Singleton<T> where T : class, new()
+    {
+
+        protected static T _instance = null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new T();
+                }
+                return _instance;
+            }
+        }
+
+        protected Singleton()
+        {
+            if(_instance!=null)
+            {
+                throw new System.Exception("This " + (typeof(T)).ToString() + " Singleton Instance is not null !!!");
+            }
+
+            Init();
+        }
+
+        protected virtual void Init()
+        {
+
+        }
+
+    }
+}
