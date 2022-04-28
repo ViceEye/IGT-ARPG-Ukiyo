b29ed3be1773d00cc189df79c13b7109710baf3d
ViceEye <ckh985169151@qq.com>
2022-04-28 21:41:48 +0100

    Cinemachine scene FOV control and Key 'G' down spawn player. Menu scene update.

---
+++ Assets/Scripts/Common/Camera/ResetFOV.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using Cinemachine;
+using UnityEngine;
+
+namespace Ukiyo.Common.Camera
+{
+    [ExecuteInEditMode]
+    public class ResetFOV : MonoBehaviour
+    {
+        public CinemachineFreeLook cinemachineFreeLookPar;
+        public GameObject player;
+        public float duration = 1.0f;
+        
+        private void Start()
+        {
+            cinemachineFreeLookPar = GetComponent<CinemachineFreeLook>();
+            cinemachineFreeLookPar.m_Lens.FieldOfView = 55;
+        }
+
+        private void Update()
+        {
+            if (!Application.isPlaying)
+            {
+                cinemachineFreeLookPar.m_Lens.FieldOfView = 55;
+            }
+            
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                player.SetActive(true);
+                StartCoroutine(PullClose());
+            }
+        }
+
+        IEnumerator PullClose()
+        {
+            var time = 0.0f;
+            while (time < duration)
+            {
+                time += Time.deltaTime;
+                cinemachineFreeLookPar.m_Lens.FieldOfView = Mathf.Lerp(55, 18, time / duration);
+                yield return new WaitForEndOfFrame();
+            }
+            cinemachineFreeLookPar.m_Lens.FieldOfView = 18;
+        }
+    }
+}
--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -84,7 +84,7 @@ namespace Ukiyo.UI
                     if (allowOpenScene)
                     {
                         // Load next scene
-                        //async.allowSceneActivation = true;
+                        async.allowSceneActivation = true;
                     }
                 }
                     
